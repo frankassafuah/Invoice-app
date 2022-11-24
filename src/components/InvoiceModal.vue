9a9bc966604a5a38874321e809a9ce71b910b206
Frank Assafuah <frankassafuah@gmail.com>
2022-11-24 23:06:19 +0000

    n

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -186,5 +186,18 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.invoice-wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: transparent;
+  width: 100%;
+  height: 100vh;
+  overflow: scroll;
+  @media(min-width: 900px){
+    left: 90px;
+  }
+
+}
 </style>
