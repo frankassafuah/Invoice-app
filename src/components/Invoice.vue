e87d76a7c833d7e3b2c58f46991eac052ddc33cb
Frank Assafuah <frankassafuah@gmail.com>
2022-11-27 22:35:55 +0000

    Retreiving firebase data

---
+++ src/components/Invoice.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Testing</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Invoice',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    created() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
