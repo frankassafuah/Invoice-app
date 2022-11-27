2dcea5e7204bad2349bdb8a429b91bfd3738c326
Frank Assafuah <frankassafuah@gmail.com>
2022-11-27 22:37:30 +0000

    Invoice component

--- src/components/Invoice.vue
+++ src/components/Invoice.vue
@@ -1,29 +1,48 @@
+
+
 <template>
-    <div>
-        <h1>Testing</h1>
-    </div>
+    <router-link class="invoice flex" :to="{ name: 'Invoice', params: { invoiceId: invoice.invoiceId } }">
+      <div class="left flex">
+        <span class="tracking-number">#{{ invoice.invoiceId }}</span>
+        <span class="due-date">{{ invoice.paymentDueDate }}</span>
+        <span class="person">{{ invoice.clientName }}</span>
+      </div>
+      <div class="right flex">
+        <span class="price">${{ invoice.invoiceTotal }}</span>
+        <div
+          class="status-button flex"
+          :class="{ paid: invoice.invoicePaid, draft: invoice.invoiceDraft, pending: invoice.invoicePending }"
+        >
+          <span v-if="invoice.invoicePaid">Paid</span>
+          <span v-if="invoice.invoiceDraft">Draft</span>
+          <span v-if="invoice.invoicePending">Pending</span>
+        </div>
+        <div class="icon">
+          <img src="@/assets/icon-arrow-right.svg" alt="" />
+        </div>
+      </div>
+    </router-link>
+  </template>
 </template>
 
 <script>
 export default {
-    name: 'Invoice',
-
-    data() {
-        return {
-            
-        };
+  name: "Invoice",
+  props: {
+    invoice: {
+      type: Object,
+      default: () => ({}),
     },
+  },
 
-    created() {
-        
-    },
+  data() {
+    return {};
+  },
 
-    methods: {
-        
-    },
+  created() {},
+
+  methods: {},
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
