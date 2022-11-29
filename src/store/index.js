bfc6a67c2186f15eef8e81fe601f1739f707782a
Frank Assafuah <frankassafuah@gmail.com>
2022-11-29 11:18:52 +0000

    Invoice view styling

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,7 @@ export default createStore({
     invoiceModal: null,
     modalActive: null,
     invoicesLoaded: null,
+    currentInvoiceArray: null,
   },
   mutations: {
     TOGGLE_INVOICE(state) {
@@ -19,6 +20,11 @@ export default createStore({
     },
     INVOICES_LOADED(state) {
       state.invoicesLoaded = true;
+    },
+    SET_CURRENT_INVOICE(state, payload) {
+      state.currentInvoiceArray = state.invoiceData.filter((invoice) => {
+        return invoice.invoiceId === payload;
+      });
     },
   },
   actions: {
